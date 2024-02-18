562fda3b2f9a705d83707b9fe852bab5d2e47235
DetailingCode <doniek77@o2.pl>
2024-02-18 21:52:02 +0100

    console log, const connect

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const name = "Dominik";
+const name = 'Piotr'
+const petName = 'Hologram'
+let petAge = 3
 
-const favMeal = "pizza";
+console.log(`Cześć, jestem ${name}, a to mój pies ${petName}, który ma już ${petAge} lata.`);
 
-console.log(`Cześć, jestem ${name} i moim ulubionym daniem jest ${favMeal}`);
