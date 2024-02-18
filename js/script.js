37057c31447f2ec08e2671db511645893a7da36f
DetailingCode <doniek77@o2.pl>
2024-02-18 22:03:31 +0100

    string type

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const name = 'Piotr'
-const petName = 'Hologram'
-let petAge = 3
+const name = "Piotr";
+const petName = "Hologram";
+let petAge = 3;
 
-console.log(`Cześć, jestem ${name}, a to mój pies ${petName}, który ma już ${petAge} lata.`);
+console.log(
+	`Cześć, jestem ${name}, a to mój pies ${petName}, który ma już ${petAge} lata.`
+);
 
+console.log(`text, "a to cudzysłów"`);
