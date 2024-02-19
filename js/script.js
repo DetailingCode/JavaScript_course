7f36699f305d98ebbcbc6d11d6ff90f8c18b1327
DetailingCode <doniek77@o2.pl>
2024-02-19 22:00:54 +0100

    tasks string

--- js/script.js
+++ js/script.js
@@ -20,3 +20,29 @@ console.log(userName.slice(1));
 
 console.log(newUserName);
 
+const text1 = "powiększ mnie";
+
+console.log(text1.toUpperCase());
+
+const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+
+console.log(text2.toLowerCase());
+
+const text3 = "##$%^ wytnij te dziwne znaki na początku";
+
+console.log(text3.slice(6));
+
+const text4 = 'sprawdź czy zawietam słowo "czy"';
+
+console.log(text4.includes("czy"));
+
+const text5 =
+	'wyLoguj w konsoli tylko literę "L", która znajduję się w wyrazie "Wyloguj';
+
+const text6 = "pies zamień każde słowo pies, kot pies";
+
+console.log(text6.replaceAll("pies", "kot"));
+
+const text7 = "podziel, ten, string, od, przecinków";
+
+console.log("%cCześć!", "font-size: 30px; color:gold; text-transform:uppercase;");
