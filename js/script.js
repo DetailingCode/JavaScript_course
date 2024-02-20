8d66751942417c7d00dce0f382fd8d1f6abad6c0
DetailingCode <doniek77@o2.pl>
2024-02-20 21:54:10 +0100

    operatory arytmetyczne

--- js/script.js
+++ js/script.js
@@ -1,60 +1,33 @@
-const name = "Piotr";
-const petName = "Hologram";
-let petAge = 3;
+const colors = ["red", "green", "blue"];
 
-console.log(
-	`Cześć, jestem ${name}, a to mój pies ${petName}, który ma już ${petAge} lata.`
-);
+console.log(colors);
 
-console.log(`text, "a to cudzysłów"`);
+function test() {
+	console.log("Funkcja!");
+}
 
-const msg = "jakaś tam wiadomość";
+test();
 
-console.log(msg.toUpperCase());
+const person = {
+	name: "imie",
+	age: 33,
+	favColor: "red",
+};
 
-const userName = "piotr";
+console.log(person);
 
-const newUserName = userName.charAt(0).toUpperCase() + userName.slice(1);
+const x = 5;
+const y = 6;
 
-console.log(userName.slice(1));
+const add = x + y;
+console.log(add);
 
-console.log(newUserName);
+const substract = x - y;
+console.log(substract);
 
-const text1 = "powiększ mnie";
+const multiply = x * y;
+console.log(multiply);
 
-console.log(text1.toUpperCase());
+const divide = x / y;
+console.log(divide);
 
-const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
-
-console.log(text2.toLowerCase());
-
-const text3 = "##$%^ wytnij te dziwne znaki na początku";
-
-console.log(text3.slice(6));
-
-const text4 = 'sprawdź czy zawietam słowo "czy"';
-
-console.log(text4.includes("czy"));
-
-const text5 =
-	'wyLoguj w konsoli tylko literę "L", która znajduję się w wyrazie "Wyloguj';
-
-console.log(text5.charAt(2));
-
-const text6 = "pies zamień każde słowo pies, kot pies";
-
-console.log(text6.replaceAll("pies", "kot"));
-
-const text7 = "podziel, ten, string, od, przecinków";
-
-console.log(text7.split(','));
-
-
-
-
-console.log("%cCześć!", "font-size: 30px; color:gold; text-transform:uppercase;");
-
-
-const num5 = 135.123
-
-console.log(num5.toFixed());
