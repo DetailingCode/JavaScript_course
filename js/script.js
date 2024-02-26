af7c998c3c3036772c2f41018cd1ec63471dcde0
DetailingCode <doniek77@o2.pl>
2024-02-26 21:41:35 +0100

    map vs forEach (difference)

--- js/script.js
+++ js/script.js
@@ -389,4 +389,10 @@ switch (day) {
 
 const numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach((number) => console.log(number * 2));
+const newNumbers = numbers.forEach(number => console.log(number * 2));
+
+console.log(newNumbers);
+
+const mapNumbers = numbers.map(number => console.log(number*2));
+console.log(mapNumbers);
+
